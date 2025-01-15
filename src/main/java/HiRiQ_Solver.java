4a14b51402b39534032f5e20bb6f4d113cdf1c91
skira99 <skira99@hotmail.com>
2025-01-14 20:58:13 -0500

    Name change to repository and main class for clarity

---
+++ src/main/java/HiRiQ_Solver.java
@@ -0,0 +1,27 @@
+package main.java;
+
+public class HiRiQ_Solver{
+
+  public static void main(String args[]) {
+    HiRiQ container = new HiRiQ((byte) 0);
+
+    // boolean[] b = new boolean[]{true, true, true,
+    //                             true, true, true,
+    //                 true, true, true, true, true, true, true,
+    //                 true, true, true, true, true, true, true,
+    //                 true, true, true, true, true, true, true,
+    //                             true, true, true,
+    //                             true, true, true};
+
+    boolean[] b = new boolean[]{false, true, true,
+                                false, false, false,
+                  false, false, false, false, false, false, false,
+                  false, false, false, false, false, false, false,
+                  false, false, false, false, false, false, false,
+                                false, false, false,
+                                false, false, false};
+
+    System.out.println("hello world");
+
+  }
+}
