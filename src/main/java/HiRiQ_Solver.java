f5d32310535cb9198c99d7f7c0683f93a1f16b80
skira99 <skira99@hotmail.com>
2025-01-19 03:05:48 -0500

    Implemented genChildren() fully.

--- src/main/java/HiRiQ_Solver.java
+++ src/main/java/HiRiQ_Solver.java
@@ -1,14 +1,18 @@
 package main.java;
+import java.util.ArrayList;
 
 public class HiRiQ_Solver{
 
+  private static  int[][] triplets = {{0,1,2}, {3,4,5}, {6,7,8}, {7,8,9}, {8,9,10}, {9,10,11}, {10,11,12}, {13,14,15}, {14,15,16}, {15,16,17}, {16,17,18}, {17,18,19}, {20,21,22}, {21,22,23}, {22,23,24}, {23,24,25}, {24,25,26}, {27,28,29}, {30,31,32}, {12,19,26}, {11,18,25}, {2,5,10}, {5,10,17}, {10,17,24}, {17,24,29}, {24,29,32}, {1,4,9}, {4,9,16}, {9,16,23}, {16,23,28}, {23,28,31}, {0,3,8}, {3,8,15}, {8,15,22}, {15,22,27}, {22,27,30}, {7,14,21}, {6,13,20}};
+  
   public static void main(String args[]) {
     HiRiQ container = new HiRiQ((byte) 0);
+    // boolean[] b =  new boolean[33];
 
     // boolean[] b = new boolean[]{true, true, true,
     //                             true, true, true,
     //                 true, true, true, true, true, true, true,
-    //                 true, true, true, true, true, true, true,
+    //                 true, true, true, false, true, true, true,
     //                 true, true, true, true, true, true, true,
     //                             true, true, true,
     //                             true, true, true};
@@ -20,8 +24,23 @@ public class HiRiQ_Solver{
                   false, false, false, false, false, false, false,
                                 false, false, false,
                                 false, false, false};
+  
+    Node test = new Node(b,"", (byte) 2);
+    test.genChildren();
+    
+    ArrayList<Node> family = new ArrayList<Node>();
+    family = test.getChildren();
+    
+    int len = family.size();
+    for(int i = 0; i < len; i++){
+      System.out.println(family.get(i).getMove());
+      container.store(family.get(i).getCurrent());
+      container.print();
+      System.out.println();
+    }
+  }
 
-    System.out.println("hello world");
-
+  public static int[] getTriplet(int index){
+    return triplets[index];
   }
 }
--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,28 +1,43 @@
 package main.java;
 import java.util.ArrayList;
 
-public class Node {
+public class Node{
   //The boolean array of the current node
   private boolean current[] = new boolean[33];
+
   //A dynamically sized array of the nodes containing the configurations reachable from the current configuration
   private ArrayList<Node> children = new ArrayList<Node>();
+
   //A string of the move taken to get from the parent configuration to this one. The solver will be called recursively and will return a string which will be the sum of the move strings to reach a solved combination
   private String move = "";
+
   //The total number of true in the current configuration
-  private byte weight = 0;
+  private byte weight;
   
   //Constructor will create a node based on input values and apply a swap if a move is specific at creation. The input weight to the constructor is the intended weight of the configuration after any input swap is applied, not the weight of the input configuration
   public Node(boolean[] config, String s, byte w){
-    if(!s.equals("")){
-      swap(config, s);
-    }
-    
-    current = config;
-    move = s;
+    //Copy the input data to the node
+    copy(config, current);
     weight = w;
+    move = s;
+
+    //If a move was specified, apply the move now and modify weight accordingly
+    if(s.length() != 0 ){
+      swap(s);
+    }
   }
 
-  public void swap(boolean[] config, String s){
+  //Function to apply a swap based on an input string of format "xx[BY]yy", where xx and yy denote the two cells on the outside of the swap, and B or W in the middle
+  //represent whether we are making a white substitution or a black substitution
+  private void swap(String s){
+    if(s.contains("W")){
+      weight--;
+    }
+
+    else if(s.contains("B")){
+      weight++;
+    }
+
     //Extract the move to be made
     String[] container = s.split("[WB]");
     
@@ -31,48 +46,93 @@ public class Node {
 
     // horizontal swap
     if(j-i == 2 || j-i == -2){
-      config[i] = !config[i];
-      config[((i+j)/2)] = !config[((i+j)/2)];
-      config[j] = !config[j];
+      current[i] = !current[i];
+      current[((i+j)/2)] = !current[((i+j)/2)];
+      current[j] = !current[j];
     }
 
     //Vertical swap for any combination that includes squares 0-6
     else if( i < 7 || j < 7 ){
-      config[i] = !config[i];
-      config[((i+j)/2)-1] = !config[((i+j)/2)-1];
-      config[j] = !config[j];
+      current[i] = !current[i];
+      current[((i+j)/2)-1] = !current[((i+j)/2)-1];
+      current[j] = !current[j];
     }
 
     //Vertical swap for any combination that includes squares 27-32
     else if(i < 26 || j < 26){
-      config[i] = !config[i];
-      config[((i+j)/2)+1] = !config[((i+j)/2)+1];
-      config[j] = !config[j];
+      current[i] = !current[i];
+      current[((i+j)/2)+1] = !current[((i+j)/2)+1];
+      current[j] = !current[j];
     }
 
     //Vertical swap for any combination in the middle
     else{
-      config[i] = !config[i];
-      config[((i+j)/2)] = !config[((i+j)/2)];
-      config[j] = !config[j];
+      current[i] = !current[i];
+      current[((i+j)/2)] = !current[((i+j)/2)];
+      current[j] = !current[j];
     }
 
     return;
   }
-    //Method to generale the list of children configurations from the current configuration
-    
+
+  //Because we can only pass the array of boolean by reference, we always have to copy the parent array to a child node by copying it 1:1
+  public void copy(boolean[] input, boolean[] target){
+    for(int i = 0; i < 33; i++){
+      target[i] = input[i];
+    }
+    return;
+  }
+
+  //Method to generale the list of children configurations from the current configuration  
   public void genChildren(){
-    if (!current[0] && current[1] & current[2]){
-      children.add(new Node(current,"2W0",(byte) (weight-1)));
+    //Pull each triplets from the main class and check all 4 possible configurations of those triplets
+    //If the configuration exists, create a node of the child configuration from applying a move
+    int i, j, k;
+    int[] triplet;
+    for(int y = 0; y < 38; y++){
+      triplet = HiRiQ_Solver.getTriplet(y);
+      i = triplet[0];
+      j = triplet[1];
+      k = triplet[2];
+
+      if(!current[i] && current[j] & current[k]){ //If false true true, which is a W sub
+        children.add(new Node(current, String.valueOf(k) + "W" + String.valueOf(i), getWeight()));
+      }
+      else if(current[i] && current[j] & !current[k]){ //If true true false, which is a W sub
+        children.add(new Node(current, String.valueOf(i) + "W" + String.valueOf(k), getWeight()));
+      }
+      else if(!current[i] && !current[j] & current[k]){ //If false false true, which is a B sub
+        children.add(new Node(current, String.valueOf(i) + "B" + String.valueOf(k), getWeight()));
+      }
+      else if(current[i] && !current[j] & !current[k]){//If true false false, which is a B sub
+        children.add(new Node(current, String.valueOf(k) + "B" + String.valueOf(i), getWeight()));
+      }
     }
   }
 
-  public boolean[] getChild(int index){
-    return children.get(index).getCurrent();
+  public Node getChild(int index){
+    return children.get(index);
+  }
+
+  public ArrayList <Node> getChildren(){
+    return children;
   }
 
   public boolean[] getCurrent(){
     return current;
+  }
+
+  public void setWeight(byte w){
+    weight = w;
+    return;
+  }
+
+  public byte getWeight(){
+    return weight;
+  }
+
+  public String getMove(){
+    return move;
   }
 
   //Test if the current configuration is solved
